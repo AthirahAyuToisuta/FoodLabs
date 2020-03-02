1dabc463d0208097a6acc1bd0f2ebe665fe9165f
Thiraa <thiraatoisuta@gmail.com>
2020-03-02 11:36:52 +0700

    First Update 3

---
+++ app/src/main/java/com/thiraa/foodlabs/home/Main3Activity.java
@@ -0,0 +1,16 @@
+package com.thiraa.foodlabs.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thiraa.foodlabs.R;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
