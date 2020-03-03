4ac36cefdda03431d5d074595e017099b06ac838
intanjoo <intanjohan1@gmail.com>
2020-03-03 17:13:49 +0700

    Merge remote-tracking branch 'origin/master' update intro
    # Conflicts:
    #	.idea/DtonatorPreferences.xml
    #	app/build.gradle
    #	app/src/main/res/layout/activity_main3.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/thiraa/foodlabs/intro/fragment/FragmentQuickSearch.java
+++ app/src/main/java/com/thiraa/foodlabs/intro/fragment/FragmentQuickSearch.java
@@ -8,23 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.thiraa.foodlabs.R;
 
 
-public class Fragment1 extends Fragment {
+public class FragmentQuickSearch extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_quick_search, container, false);
-        Button buttonInFragment1 = rootView.findViewById(R.id.button_1);
-        buttonInFragment1.(new View.OnClickListener() {
+        Button btnLogin = rootView.findViewById(R.id.btnLogin1);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "button in fragment 1", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Quick Search", Toast.LENGTH_SHORT).show();
             }
-        });
+            });
+
 
         return rootView;
     }
--- app/src/main/java/com/thiraa/foodlabs/intro/models/ViewPagerAdapter.java
+++ app/src/main/java/com/thiraa/foodlabs/intro/models/ViewPagerAdapter.java
@@ -1,21 +1,23 @@
-package com.thiraa.foodlabs.intro.adapter;
+package com.thiraa.foodlabs.intro.models;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 import com.thiraa.foodlabs.intro.fragment.FragmentFastShipping;
 import com.thiraa.foodlabs.intro.fragment.FragmentQuickSearch;
 import com.thiraa.foodlabs.intro.fragment.FragmentSearchPlace;
 import com.thiraa.foodlabs.intro.fragment.FragmentVarietyFood;
 
-public class ViewPagerAdapter {
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
     private Fragment[] introFragments;
 
     public ViewPagerAdapter(FragmentManager fm) {
         super(fm);
         introFragments = new Fragment[] {
-                new FragmentFastShipping(), //0
-                new FragmentQuickSearch(), //1
+                new FragmentFastShipping(),
+                new FragmentQuickSearch(),
                 new FragmentSearchPlace(),
                 new FragmentVarietyFood()
         };
