8eb7b3efd60942f66544afeb457e307c3a562895
Thiraa <thiraatoisuta@gmail.com>
2020-03-10 11:13:37 +0700

    Design tambahan login, profile, location

---
+++ app/src/main/java/com/thiraa/foodlabs/login_regis/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.thiraa.foodlabs.login_regis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
