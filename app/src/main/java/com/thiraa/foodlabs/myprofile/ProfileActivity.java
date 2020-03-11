1ac9de2bd7de2e2d5573b8aebdfdd7973e1cc19d
intanjoo <intanjohan1@gmail.com>
2020-03-11 15:49:55 +0700

    Merge remote-tracking branch 'origin/master' update intro done1
    # Conflicts:
    #	.idea/DtonatorPreferences.xml
    #	app/build.gradle
    #	app/src/main/res/layout/activity_main3.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/thiraa/foodlabs/myprofile/ProfileActivity.java
+++ app/src/main/java/com/thiraa/foodlabs/myprofile/ProfileActivity.java
@@ -1,14 +1,23 @@
 package com.thiraa.foodlabs.myprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.thiraa.foodlabs.R;
+import com.thiraa.foodlabs.intro.models.ViewPagerAdapter;
+
 public class ProfileActivity extends AppCompatActivity {
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
     }
 }
